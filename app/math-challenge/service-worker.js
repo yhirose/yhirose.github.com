45b813cfc1ad05340c518e54792c36d24ae15561
yhirose <yuji.hirose.bug@gmail.com>
2021-07-24 23:08:21 -0400

    Removed app

--- app/math-challenge/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-});
-
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-});
-
-self.addEventListener('fetch', function(event) {});
